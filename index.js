3c97bf8c3db6a48ac18be57eb3b5b677700d2178
Kevin Mejía <barraganosvaldo39@gmail.com>
2023-09-09 19:21:36 -0400

    Add a text wrapper in the infoCanvas

--- index.js
+++ index.js
@@ -27,6 +27,44 @@ export const buttons = {
     }
 };
 
+// Shout out to the FJOLT website for this function
+// If you want to find out more stuff like this, it is part of their HTML Canvas series
+export const wrapText = function(ctx, text, x, y, maxWidth, lineHeight) {
+    // First, start by splitting all of our text into words, but splitting it into an array split by spaces
+    let words = text.split(' ');
+    let line = ''; // This will store the text of the current line
+    let testLine = ''; // This will store the text when we add a word, to test if it's too long
+    let lineArray = []; // This is an array of lines, which the function will return
+
+    // Lets iterate over each word
+    for(var n = 0; n < words.length; n++) {
+        // Create a test line, and measure it..
+        testLine += `${words[n]} `;
+        let metrics = ctx.measureText(testLine);
+        let testWidth = metrics.width;
+        // If the width of this test line is more than the max width
+        if (testWidth > maxWidth && n > 0) {
+            // Then the line is finished, push the current line into "lineArray"
+            lineArray.push([line, x, y]);
+            // Increase the line height, so a new line is started
+            y += lineHeight;
+            // Update line and test line to use this word as the first word on the next line
+            line = `${words[n]} `;
+            testLine = `${words[n]} `;
+        }
+        else {
+            // If the test line is still less than the max width, then add the word to the current line
+            line += `${words[n]} `;
+        }
+        // If we never reach the full max width, then there is only one line.. so push it into the lineArray so we return something
+        if(n === words.length - 1) {
+            lineArray.push([line, x, y]);
+        }
+    }
+    // Return the line array
+    return lineArray;
+}
+
 function animate() {
     requestAnimationFrame(animate);
     ctx.fillStyle = "black";
@@ -65,10 +103,15 @@ function animateInfo() {
                     character.pos.x <= door.pos.x + door.width &&
                     character.pos.y <= door.pos.y + door.height &&
                     character.pos.y + character.height >= door.pos.y) {
-                        infoCtx.font = "20px Georgia";
+                        infoCtx.font = "30px Georgia";
                         infoCtx.fillStyle = "black";
-                        infoCtx.fillText("Click (Space) to Enter " + door.goesTo.name, 0, 20);
-                    }
+                        // infoCtx.fillText("Click (Space) to Enter " + door.goesTo.name, 0, 20);
+                        let wrappedText = wrapText(infoCtx, "Click (Space) to Enter " + door.goesTo.name, 0, 30, 576, 30);
+                        wrappedText.forEach(function(item) {
+                            // item[0] is the text, item[1] is the x coordinate, and item[2] is the y coordinate
+                            infoCtx.fillText(item[0], item[1], item[2]);
+                        })
+                }
             })
         }
     }
