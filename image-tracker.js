b4a69d15090f91600cd4daf94aa46244d864e5ea
Kevin Mejía <barraganosvaldo39@gmail.com>
2023-09-09 15:38:08 -0400

    Image source was wrong. It is now fixed.

--- image-tracker.js
+++ image-tracker.js
@@ -1,5 +1,5 @@
 // Export Background Images Here
 // Export Building Images Here
 export const home1_image = new Image();
-home1_image.src = "./Images/buildings/home1.png";
+home1_image.src = "./Images/buildings/Home1.png";
 // Export Character Images Here
